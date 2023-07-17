006f1b673d2d990053003e210a571b7b2e4707c7
huaerjiejue <emtric.cn@gmail.com>
2023-07-17 09:47:56 +0800

    delete some files useless

--- background.js
+++ background.js
@@ -3,3 +3,54 @@
 调用翻译API接口
 接收翻译结果,将结果插入页面
  */
+// background.js
+
+// 导入翻译函数
+import {translateText} from './translate.js';
+
+// 定义chrome变量
+const chrome = window.chrome || window.browser;
+
+// 监听内容脚本消息
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+  // 如果是翻译请求
+  if(request.type === 'translate'){
+
+    // 获取用户输入内容
+    const input = request.text;
+
+    // 检查是否有翻译触发词"/e"
+    const hasTrigger = input.endsWith('/e');
+
+    // 如果有触发词
+    if(hasTrigger){
+
+      // 切掉触发词获取需要翻译的文本
+      const textToTranslate = input.slice(0, -2);
+
+      // 调用翻译函数获得翻译结果
+      const translated = translateText(textToTranslate);
+
+      // 返回翻译结果给内容脚本
+      sendResponse({translated});
+    }
+  }
+});
+
+// 监听存储值更改
+chrome.storage.onChanged.addListener((changes) => {
+
+  // 如果是自动翻译设置更改
+  if(changes.autoTranslate){
+
+    // 获取当前活动tab
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+
+      // 发送自动翻译设置到内容脚本
+      chrome.tabs.sendMessage(tabs[0].id, {
+        autoTranslate: changes.autoTranslate.newValue
+      });
+    });
+  }
+})
--- contentScript.js
+++ contentScript.js
@@ -2,4 +2,47 @@
 获取输入框内容
 将翻译结果插入输入框
 实现中英文切换
- */
+*/
+
+// 定义变量
+const inputElement = document.querySelector('input');
+let translatedText = '';
+const chrome = window.chrome || window.browser;
+
+// 监听背景页消息
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  // 如果接收到翻译结果
+  if(request.translated){
+    // 保存结果
+    translatedText = request.translated;
+    // 插入输入框
+    inputElement.value = translatedText;
+  }
+
+  // 如果是自动翻译设置
+  if(request.autoTranslate){
+    // 更新设置
+    isAutoTranslate = request.autoTranslate;
+  }
+});
+
+// 监听输入框输入
+inputElement.addEventListener('input', () => {
+  // 如果自动翻译打开
+  if(isAutoTranslate){
+     // 发送翻译请求到背景页
+     chrome.runtime.sendMessage({
+       type: 'translate',
+       text: inputElement.value
+     });
+  }
+});
+
+// 中英文切换
+document.addEventListener('keydown', (e) => {
+  if(e.code === 'Space' && e.ctrlKey){
+     // 切换中英文
+     inputElement.value = inputElement.value === translatedText
+       ? translatedText : translatedText;
+  }
+});
